475f35252f98e4a085b4a5197a749ecd6f9b9fa1
otto-dlh <ottodlh@live.com>
2024-10-19 22:54:03 -0600

    agrega texto invitado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,88 @@
 (function ($) {
     "use strict";
+    
+    //constantes
     const fechaObjetivo = new Date('2025-01-18T00:00:00').getTime();
+    const data = [
+        { nombre: "Ottoniel de León", valor: 1 },
+        { nombre: "Ligia Chanquín", valor: 1 },
+        { nombre: "Ana Villeda", valor: 1 },
+        { nombre: "Melissa Chanquín", valor: 1 },
+        { nombre: "Alexander Sagastume", valor: 1 },
+        { nombre: "Herberth Chanquín", valor: 1 },
+        { nombre: "Familia Paz", valor: 2 },
+        { nombre: "Familia Castillo", valor: 2 },
+        { nombre: "Familia Ángel Valdez", valor: 4 },
+        { nombre: "Familia Nancy Girón", valor: 3 },
+        { nombre: "Silvia González", valor: 1 },
+        { nombre: "María José", valor: 1 },
+        { nombre: "Daniela Vásquez", valor: 2 },
+        { nombre: "Dulce Noriega", valor: 1 },
+        { nombre: "Thelma Palencia", valor: 2 },
+        { nombre: "Esdras Samayoa", valor: 1 },
+        { nombre: "Diana Vásquez", valor: 1 },
+        { nombre: "Anneth", valor: 1 },
+        { nombre: "Team Maestría", valor: 4 },
+        { nombre: "Yasmín Vásquez", valor: 1 },
+        { nombre: "Catherine Pineda", valor: 1 },
+        { nombre: "Kristal de León", valor: 1 },
+        { nombre: "Mishelle Lecointe", valor: 2 },
+        { nombre: "Familia Mejía", valor: 3 },
+        { nombre: "Familia Mimi", valor: 2 },
+        { nombre: "Anabelly Hernández", valor: 1 },
+        { nombre: "Miranda Matías", valor: 1 },
+        { nombre: "Jeannifer Pérez", valor: 1 },
+        { nombre: "Bryant Bran", valor: 1 },
+        { nombre: "Alex García", valor: 1 },
+        { nombre: "Dhaffnne Arriola", valor: 1 },
+        { nombre: "Lourdes Cruz", valor: 2 },
+        { nombre: "Evelyn Najera", valor: 1 },
+        { nombre: "Mónica Castellanos", valor: 1 },
+        { nombre: "Saulo Camey", valor: 1 },
+        { nombre: "Evelyn Castellanos", valor: 2 },
+        { nombre: "Familia Erick Pineda", valor: 2 },
+        { nombre: "Familia Tomás Avendaño", valor: 4 },
+        { nombre: "Familia Juan Echeverría", valor: 2 },
+        { nombre: "Familia Manolo Echeverría", valor: 2 },
+        { nombre: "Familia Jennifer Avendaño", valor: 2 },
+        { nombre: "Familia Rivera", valor: 4 },
+        { nombre: "Mauricio Sánchez", valor: 1 },
+        { nombre: "Jeshua Uribio", valor: 1 },
+        { nombre: "Gerson Rodríguez", valor: 2 },
+        { nombre: "Kathy y Daniel", valor: 2 },
+        { nombre: "Familia Sergio Barrios", valor: 2 },
+        { nombre: "Familia Sara Donis", valor: 3 },
+        { nombre: "Jose Díaz", valor: 1 },
+        { nombre: "Ottoniel de León Natareno", valor: 1 },
+        { nombre: "Reyna Hernández", valor: 1 },
+        { nombre: "Lesly de León", valor: 2 },
+        { nombre: "Daniel de León", valor: 1 },
+        { nombre: "Familia Mildred de León", valor: 4 },
+        { nombre: "Familia Elí de León", valor: 4 },
+        { nombre: "Familia Maynor de León", valor: 4 },
+        { nombre: "Familia Ceci de León", valor: 3 },
+        { nombre: "Familia Joel de León", valor: 4 },
+        { nombre: "Familia Marlon de León", valor: 2 },
+        { nombre: "Familia Analí de León", valor: 3 },
+        { nombre: "Familia Caro de León", valor: 4 },
+        { nombre: "Familia Noly de León", valor: 5 },
+        { nombre: "Familia Antonia de León", valor: 2 },
+        { nombre: "Sonia Salvatierra", valor: 3 },
+        { nombre: "Familia David Gonzáles", valor: 4 },
+        { nombre: "Familia Alan Ortega", valor: 3 },
+        { nombre: "Edu Gonzales", valor: 1 },
+        { nombre: "Edgar Alvarado", valor: 1 },
+        { nombre: "Edwin Ixcoy", valor: 2 },
+        { nombre: "Cristian Florian", valor: 1 },
+        { nombre: "Diego Taracena", valor: 2 },
+        { nombre: "José Urias", valor: 1 },
+        { nombre: "Antulio Morales", valor: 2 },
+        { nombre: "Soly", valor: 1 },
+        { nombre: "Doña Xiomara", valor: 2 },
+        { nombre: "Vaneska Corado", valor: 2 }
+      ];
+      
+      
     $(document).ready(function () {
         function actualizarReloj() {
             const ahora = new Date().getTime();
@@ -30,6 +112,23 @@
 
         // Actualizar cada segundo
         const intervalo = setInterval(actualizarReloj, 1000);
+
+        // Función para obtener el valor del query string
+        function getQueryParameter(name) {
+            const urlParams = new URLSearchParams(window.location.search);
+            return urlParams.get(name);
+        }
+
+        // Obtener el número del query string
+        const numero = getQueryParameter('v');
+
+        // Encontrar el objeto correspondiente al número
+        const resultado = data[numero - 1]; // restamos 1 ya que los índices del array empiezan en 0
+
+        // Mostrar el resultado en la página
+        if (resultado) {
+            document.getElementById('resultado').innerText = ` ${resultado.nombre} \n Cantidad de personas: ${resultado.valor}`;
+        }
     });
     /*function actualizarContador() {
         const fechaObjetivo = new Date('2025-01-18T00:00:00');
